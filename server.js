f6078dfffb3ed8bed4ed1d9fe1b1f6df190a1927
tanmaydixit27 <82702135+tanmaydixit27@users.noreply.github.com>
2024-12-17 18:25:59 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,41 +1,46 @@
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
-const connectDB = require('./config/db');
 const mongoose = require('mongoose');
+const connectDB = require('./config/db');
 const authRoutes = require('./routes/authRoutes');
 const grainsRoutes = require('./routes/grainsRoutes');
 const cartRoutes = require('./routes/cartRoutes');
 const eventRoutes = require('./routes/eventRoutes');
-
 
 dotenv.config();
 const app = express();
 
 // Middleware
 app.use(express.json());
-app.use(cors());
 
+
+const corsOptions = {
+  origin: 'https://annadaata.netlify.app',
+  methods: 'GET,POST,PUT,DELETE',          
+  credentials: true,                      
+};
+
+app.use(cors(corsOptions));
+
+// API Routes
+app.use('/api/auth', authRoutes);
 app.use('/api/grains', grainsRoutes);
 app.use('/api/cart', cartRoutes);
 app.use('/api/events', eventRoutes);
 
-
+// MongoDB Connection
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('MongoDB connected'))
-  .catch(err => console.log(err));
+  .catch(err => console.error('MongoDB connection error:', err));
 
-app.use('/api/auth', authRoutes);
-
-
+// Root route
 app.get('/', (req, res) => {
   res.send('Welcome to the Anndaata API');
 });
 
-
+// Server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
-
-
