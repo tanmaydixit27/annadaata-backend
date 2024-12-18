0ed8626f29dbdccb790116c98928d59c209c6bda
tanmaydixit27 <tanmaydixit7054@gmail.com>
2024-12-19 03:40:10 +0530

    second commit in user

--- models/userModel.js
+++ models/userModel.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 
-onst userSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema({
   fullName: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
